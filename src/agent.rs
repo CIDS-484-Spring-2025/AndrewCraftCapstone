6f846169d2153b46dfc5d2f6f59f42a296aabe81
andcraft000 <andcraft000@gmail.com>
2025-04-21 21:36:16 -0500

    added basic ML outline to work on

---
+++ src/agent.rs
@@ -0,0 +1,52 @@
+mod agent;
+
+use agent::Agent;
+use agent::{Agent, train_agent};
+use std::collections::HashMap;
+use rand::seq::SliceRandom;
+use rand::Rng;
+
+type State = (usize, usize); // x, y
+type Action = usize; // 0 = up, 1 = down, 2 = left, 3 = right
+const ACTIONS: [Action; 4] = [0, 1, 2, 3];
+
+let mut q_table: HashMap<(State, Action), f32> = HashMap::new();
+
+/*
+Quick explanation of alpha, gamma, and epsilon here.
+Alpha is the learning rate; Determines how much new information overrides old knowledge in the Q-Table.
+    so alpha = 1 would mean it always fully trusts new info 
+     while 0 makes it never update and always sticks to what it knows
+Gamma determines how much this agent cares about future rewards vs immediate rewards
+    gamma = 0 means it only cares about immediate rewards and gamma = 1 means it values long term rewards equally
+Epsilon controls how often our agent takes a random action instead of the one it thinks is best
+    epsilon = 0.0 -> never explores; epsilon = 1.0 -> random moves
+and we store all of this within a HaseMap
+*/
+#[derive(Debug)]
+pub struct Agent {
+    pub x: usize,
+    pub y: usize,
+    pub q_table: HashMap<(State, Action), f32>,
+    pub alpha: f32, // learning rate
+    pub gamma: f32, // discount factor
+    pub epsilon: f32, // exploration rate
+}
+
+impl Agent {
+    pub fn new(start_x: usize, start_y: usize) -> Self {
+        Self { x: start_x, y: start_y }
+    }
+
+    pub fn reset(&mut self, start_x: usize, start_y: usize) {
+        self.x = start_x;
+        self.y = start_y;
+    }
+
+    // TODO add movement, decision making, and other things here
+}
+
+pub fn train_agent() {
+    println!("Training agent...");
+    // TODO: implement Q-learning here
+}
